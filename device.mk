7a0fabc66c00d5854e606eaad2370602201e7c2a
johnmart19 <johnivan19999@gmail.com>
2022-11-06 06:04:07 +0200

    munch: Import Dolby Config
    - From Miui 14.0.2.0 EU
    - Also disabled Volume Leveler

--- device.mk
+++ device.mk
@@ -13,6 +13,10 @@ $(call inherit-product, device/xiaomi/sm8250-common/kona.mk)
 # Dolby Support
 TARGET_USES_MIUI_DOLBY := true
 
+# Dolby Config File
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/dolby/config/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
